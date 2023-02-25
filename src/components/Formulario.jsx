e0611ca0c100575f829454f1cb0a9dda58be4174
Fede <federicoameratte@gmail.com>
2023-02-25 03:49:51 -0300

    completando formulario y listado de pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,30 +1,105 @@
+import { useState, useEffect } from "react";
 
 const Formulario = () => {
+
+  const [mascota, setMascota] = useState({})
+  const [propietario, setPropietario] = useState({})
+  const [email, setEmail] = useState({})
+  const [date, setDate] = useState({})
+
+
+
   return (
     <div className="md:w-1/2 lg-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-    
-      <p className="text-lg mt-5 text-center">
-        Añade pacientes y {''}
+
+      <p className="text-lg mt-5 mb-10 text-center">
+        Añade pacientes y {""}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
-        <div>
-          <label htmlFor="mascota" className="block text-grey text-gray-700 font-bold uppercase">
-            Nombre macota
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-grey text-gray-700 font-bold uppercase"
+          >
+            Nombre Mascota
           </label>
 
-          <input 
+          <input
             id="mascota"
-            type="text" 
+            type="text"
             placeholder="Nombre de Mascota"
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"  
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           />
         </div>
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-grey text-gray-700 font-bold uppercase"
+          >
+            Nombre Propietario
+          </label>
+
+          <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-grey text-gray-700 font-bold uppercase"
+          >
+            Email
+          </label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Email "
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-grey text-gray-700 font-bold uppercase"
+          >
+            Alta
+          </label>
+
+          <input
+            id="alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-grey text-gray-700 font-bold uppercase"
+          >
+            Sintomas
+          </label>
+
+          <textarea
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Escribe los Sintomas"
+          ></textarea>
+        </div>
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-xl hover:bg-indigo-700 hover:cursor-pointer transition-all"
+          value="Agregar Paciente"
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,9 +1,17 @@
+import Paciente from "./paciente"
 
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2">
-      <h2>ListadoPacientes</h2>
-      
+    <div className="md:w-1/2 lg:w-3/5  md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+      <p className="text-lg mt-5 mb-10 text-center ">
+        Admimistra tus {''} 
+        <span className="text-indigo-600 font-bold"> Pacientes y Citas</span>
+      </p>
+      <Paciente/> 
+      <Paciente/> 
+      <Paciente/> 
+      <Paciente/> 
     </div>
   )
 }
